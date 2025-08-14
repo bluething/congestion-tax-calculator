880d97ff5e3ac7a5bacdf2457fdf3f919f6d7144
Habib Ali Machpud <habib.machpud@gmail.com>
2025-08-14 22:06:06 +0700

    Spring Boot entry point

---
+++ src/main/java/io/github/bluething/congestion/calculator/CongestionTaxApplication.java
@@ -0,0 +1,11 @@
+package io.github.bluething.congestion.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongestionTaxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CongestionTaxApplication.class, args);
+    }
+}
